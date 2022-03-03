17e7e989ba7c9dd9aad66e33389ef0e4a7be4d72
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-03 15:56:22 -0300

    feat: config with PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Next.js Template</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
           content="Next.js template with typescript, jest, styled-components"
